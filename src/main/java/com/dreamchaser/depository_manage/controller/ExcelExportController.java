226a0adffd7ebb8e3106043241e25c380f6bf35b
Q <878576256@qq.com>
2023-08-25 09:44:15 +0800

    8.25

--- src/main/java/com/dreamchaser/depository_manage/controller/ExcelExportController.java
+++ src/main/java/com/dreamchaser/depository_manage/controller/ExcelExportController.java
@@ -17,6 +17,7 @@ import java.io.ByteArrayOutputStream;
 import java.net.URLEncoder;
 import java.util.Map;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/export")
@@ -120,28 +121,57 @@ public class ExcelExportController {
         List<Map<String, Object>> data = reportService.transferData(year, month);
         XSSFWorkbook workbook = new XSSFWorkbook();
         XSSFSheet sheet = workbook.createSheet("Report");
-        // Create header row
-        XSSFRow headerRow = sheet.createRow(0);
-        headerRow.createCell(0).setCellValue("日期");
-        headerRow.createCell(1).setCellValue("品名");
-        headerRow.createCell(2).setCellValue("型号");
-        headerRow.createCell(3).setCellValue("单价");
-        headerRow.createCell(4).setCellValue("数量");
-        headerRow.createCell(5).setCellValue("总价");
-        headerRow.createCell(6).setCellValue("备注");
+//        // Create header row
+//        XSSFRow headerRow = sheet.createRow(0);
+//        headerRow.createCell(0).setCellValue("日期");
+//        headerRow.createCell(1).setCellValue("品名");
+//        headerRow.createCell(2).setCellValue("型号");
+//        headerRow.createCell(3).setCellValue("单价");
+//        headerRow.createCell(4).setCellValue("数量");
+//        headerRow.createCell(5).setCellValue("总价");
+//        headerRow.createCell(6).setCellValue("备注");
+//
+//        for (int i = 0; i < data.size(); i++) {
+//            Map<String, Object> record = data.get(i);
+//            XSSFRow row = sheet.createRow(i + 1);
+//            row.createCell(0).setCellValue((String) getOrDefault(record, "日期", ""));
+//            row.createCell(1).setCellValue((String) getOrDefault(record, "品名", "aaa"));
+//            row.createCell(2).setCellValue((String) getOrDefault(record, "型号", "aaa"));
+//            row.createCell(3).setCellValue(((Double) getOrDefault(record, "单价", 0.0)).doubleValue());
+//            row.createCell(4).setCellValue(((Double) getOrDefault(record, "数量", 0.0)).doubleValue());
+//            row.createCell(5).setCellValue(((Double) getOrDefault(record, "总价", 0.0)).doubleValue());
+//            row.createCell(6).setCellValue((String) getOrDefault(record, "备注", "aaa"));
+//
+//        }
+        // ... [前面的代码不变]
 
-        for (int i = 0; i < data.size(); i++) {
-            Map<String, Object> record = data.get(i);
-            XSSFRow row = sheet.createRow(i + 1);
-            row.createCell(0).setCellValue((String) getOrDefault(record, "日期", ""));
-            row.createCell(1).setCellValue((String) getOrDefault(record, "品名", "aaa"));
-            row.createCell(2).setCellValue((String) getOrDefault(record, "型号", "aaa"));
-            row.createCell(3).setCellValue(((Double) getOrDefault(record, "单价", 0.0)).doubleValue());
-            row.createCell(4).setCellValue(((Double) getOrDefault(record, "数量", 0.0)).doubleValue());
-            row.createCell(5).setCellValue(((Double) getOrDefault(record, "总价", 0.0)).doubleValue());
-            row.createCell(6).setCellValue((String) getOrDefault(record, "备注", "aaa"));
+// 分割数据
+        List<Map<String, Object>> zabToSabData = data.stream()
+                .filter(record -> "ZAB转入SAB".equals(record.get("备注")))
+                .collect(Collectors.toList());
+        List<Map<String, Object>> sabToZabData = data.stream()
+                .filter(record -> "SAB转入ZAB".equals(record.get("备注")))
+                .collect(Collectors.toList());
 
-        }
+// 创建 ZAB向SAB部分
+        int currentRowIndex = 0;
+        XSSFRow titleRowZabToSab = sheet.createRow(currentRowIndex++);
+        titleRowZabToSab.createCell(0).setCellValue("SAB向ZAB借用物资清单");
+        currentRowIndex += 2; // 留空一行
+        createHeaderRow(sheet, currentRowIndex++);
+        currentRowIndex = fillDataRows(sheet, currentRowIndex, zabToSabData);
+
+        currentRowIndex += 2; // 留空一行
+
+// 创建 SAB向ZAB部分
+        XSSFRow titleRowSabToZab = sheet.createRow(currentRowIndex++);
+        titleRowSabToZab.createCell(0).setCellValue("ZAB向SAB借用物资清单");
+        currentRowIndex += 2; // 留空一行
+        createHeaderRow(sheet, currentRowIndex++);
+        fillDataRows(sheet, currentRowIndex, sabToZabData);
+
+// ... [后面的代码不变]
+
 
         ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
         workbook.write(outputStream);
@@ -153,6 +183,31 @@ public class ExcelExportController {
         return ResponseEntity.ok()
                 .header("Content-Disposition", "attachment; filename*=UTF-8''" + encodedFilename)
                 .body(resource);
+    }
+    private void createHeaderRow(XSSFSheet sheet, int rowIndex) {
+        XSSFRow headerRow = sheet.createRow(rowIndex);
+        headerRow.createCell(0).setCellValue("日期");
+        headerRow.createCell(1).setCellValue("品名");
+        headerRow.createCell(2).setCellValue("型号");
+        headerRow.createCell(3).setCellValue("单价");
+        headerRow.createCell(4).setCellValue("数量");
+        headerRow.createCell(5).setCellValue("总价");
+        headerRow.createCell(6).setCellValue("备注");
+    }
+
+    private int fillDataRows(XSSFSheet sheet, int startingRowIndex, List<Map<String, Object>> data) {
+        int rowIndex = startingRowIndex;
+        for (Map<String, Object> record : data) {
+            XSSFRow row = sheet.createRow(rowIndex++);
+            row.createCell(0).setCellValue((String) getOrDefault(record, "日期", ""));
+            row.createCell(1).setCellValue((String) getOrDefault(record, "品名", "aaa"));
+            row.createCell(2).setCellValue((String) getOrDefault(record, "型号", "aaa"));
+            row.createCell(3).setCellValue(((Double) getOrDefault(record, "单价", 0.0)).doubleValue());
+            row.createCell(4).setCellValue(((Double) getOrDefault(record, "数量", 0.0)).doubleValue());
+            row.createCell(5).setCellValue(((Double) getOrDefault(record, "总价", 0.0)).doubleValue());
+            row.createCell(6).setCellValue((String) getOrDefault(record, "备注", "aaa"));
+        }
+        return rowIndex;
     }
 
     private Object getOrDefault(Map<String, Object> map, String key, Object defaultValue) {
