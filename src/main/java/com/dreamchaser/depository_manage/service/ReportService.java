430996db4a65711528ee6a9a01f553e120fe8afb
queshumin <shuminque@gmail.com>
2023-08-28 09:37:41 +0800

    8.28(pc)

--- src/main/java/com/dreamchaser/depository_manage/service/ReportService.java
+++ src/main/java/com/dreamchaser/depository_manage/service/ReportService.java
@@ -48,34 +48,35 @@ public class ReportService {
                         "        mt.id AS type_id,\n" +
                         "        CASE WHEN mt.tname LIKE '%(进口)' THEN 1 ELSE 0 END AS is_import,\n" +
                         "        mt.tname AS material_type_name,\n" +
-                        "        COALESCE(SUM(CASE WHEN dr.type = 1 THEN dr.price * dr.quantity ELSE 0 END), 0) AS 入库金额,\n" +
-                        "        COALESCE(SUM(CASE WHEN dr.type = 0 THEN dr.price * dr.quantity ELSE 0 END), 0) AS 出库金额,\n" +
+                        "        COALESCE(SUM(CASE WHEN dr.type = 1 THEN dr.price * dr.quantity ELSE 0 END), 0) AS raw_in_money,\n" +
+                        "        COALESCE(SUM(CASE WHEN dr.type = 0 THEN dr.price * dr.quantity ELSE 0 END), 0) AS raw_out_money,\n" +
+                        "        FORMAT(COALESCE(SUM(CASE WHEN dr.type = 1 THEN dr.price * dr.quantity ELSE 0 END), 0), 2) AS 入库金额,\n" +
+                        "        FORMAT(COALESCE(SUM(CASE WHEN dr.type = 0 THEN dr.price * dr.quantity ELSE 0 END), 0), 2) AS 出库金额,\n" +
                         "        IFNULL(YEAR(dr.review_time), ?) AS report_year,\n" +
                         "        IFNULL(MONTH(dr.review_time), ?) AS report_month\n" +
                         "    FROM material_type mt\n" +
                         "    LEFT JOIN depository_record dr ON mt.tname = dr.type_name\n" +
-                        "                               AND MONTH(dr.review_time) = ?" +
-                        "                               AND YEAR(dr.review_time) = ?" +
-                        "                               AND dr.review_pass = 1" +
-                        "                               AND dr.depository_id = ?" +
+                        "                               AND MONTH(dr.review_time) = ?\n" +
+                        "                               AND YEAR(dr.review_time) = ?\n" +
+                        "                               AND dr.review_pass = 1\n" +
+                        "                               AND dr.depository_id = ?\n" +
                         "    GROUP BY mt.tname, mt.id, report_year, report_month\n" +
                         "),\n" +
-                        "\n" +
                         "StockValue AS (\n" +
                         "    SELECT\n" +
                         "        mt.id AS type_id,\n" +
-                        "        SUM(m.price) AS 在库金额,\n" +
+                        "        SUM(m.price) AS raw_stock_money,\n" +
+                        "        FORMAT(SUM(m.price), 2) AS 在库金额,\n" +
                         "        CASE WHEN mt.tname LIKE '%(进口)' THEN 1 ELSE 0 END AS is_import\n" +
                         "    FROM material_type mt\n" +
                         "    LEFT JOIN material m ON mt.id = m.type_id\n" +
-                        "    WHERE m.depository_id = ?" +
+                        "    WHERE m.depository_id = ?\n" +
                         "    GROUP BY mt.id\n" +
                         "),\n" +
-                        "\n" +
                         "FinalData AS (\n" +
                         "    SELECT\n" +
                         "        CONCAT(cd.report_year, '/', cd.report_month) AS 日期,\n" +
-                        "        cd.material_type_name AS 分类,\n" +
+                        "        cd.material_type_name AS 材料类型,\n" +
                         "        cd.入库金额,\n" +
                         "        cd.出库金额,\n" +
                         "        COALESCE(s.在库金额, 0) AS 在库金额,\n" +
@@ -83,49 +84,29 @@ public class ReportService {
                         "        cd.type_id\n" +
                         "    FROM CombinedData cd\n" +
                         "    LEFT JOIN StockValue s ON cd.type_id = s.type_id\n" +
-                        "    WHERE cd.is_import = 1\n" +
-                        "\n" +
-                        "    UNION ALL\n" +
-                        "\n" +
-                        "    SELECT\n" +
-                        "        CONCAT(cd.report_year, '/', cd.report_month) AS 日期,\n" +
-                        "        cd.material_type_name AS 分类,\n" +
-                        "        cd.入库金额,\n" +
-                        "        cd.出库金额,\n" +
-                        "        COALESCE(s.在库金额, 0) AS 在库金额,\n" +
-                        "        cd.is_import,\n" +
-                        "        cd.type_id\n" +
-                        "    FROM CombinedData cd\n" +
-                        "    LEFT JOIN StockValue s ON cd.type_id = s.type_id\n" +
-                        "    WHERE cd.is_import = 0\n" +
                         "),\n" +
-                        "\n" +
                         "Totals AS (\n" +
                         "    SELECT\n" +
                         "        '总计' AS 日期,\n" +
-                        "        ' ' AS 分类,\n" +
-                        "        SUM(cd.入库金额) AS 入库金额,\n" +
-                        "        SUM(cd.出库金额) AS 出库金额,\n" +
-                        "        SUM(COALESCE(s.在库金额, 0)) AS 在库金额,\n" +
+                        "        ' ' AS 材料类型,\n" +
+                        "        FORMAT(SUM(cd.raw_in_money), 2) AS 入库金额,\n" +
+                        "        FORMAT(SUM(cd.raw_out_money), 2) AS 出库金额,\n" +
+                        "        FORMAT(SUM(COALESCE(s.raw_stock_money, 0)), 2) AS 在库金额,\n" +
                         "        cd.is_import,\n" +
                         "        9999 AS type_id\n" +
                         "    FROM CombinedData cd\n" +
                         "    LEFT JOIN StockValue s ON cd.type_id = s.type_id\n" +
-                        "    GROUP BY cd.report_year, cd.report_month, cd.is_import\n" +
+                        "    GROUP BY cd.is_import\n" +
                         ")\n" +
-                        "\n" +
-                        "SELECT 日期, 分类, 入库金额, 出库金额, 在库金额\n" +
+                        "SELECT 日期, 材料类型, 入库金额, 出库金额, 在库金额\n" +
                         "FROM (\n" +
-                        "    SELECT 日期, 分类, 入库金额, 出库金额, 在库金额, is_import, type_id\n" +
+                        "    SELECT 日期, 材料类型, 入库金额, 出库金额, 在库金额, is_import, type_id\n" +
                         "    FROM FinalData\n" +
-                        "\n" +
                         "    UNION ALL\n" +
-                        "\n" +
-                        "    SELECT 日期, 分类, 入库金额, 出库金额, 在库金额, is_import, type_id\n" +
+                        "    SELECT 日期, 材料类型, 入库金额, 出库金额, 在库金额, is_import, type_id\n" +
                         "    FROM Totals\n" +
                         ") AS SubQuery\n" +
                         "ORDER BY is_import DESC, type_id ASC, 日期 DESC;";
-
         return jdbcTemplate.queryForList(sql, year, month, month, year, depositoryId, depositoryId);
     }
 
