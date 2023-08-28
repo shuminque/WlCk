51073265f95cbdf9d8bf7580ab7afbfcc1fe4aee
Q <878576256@qq.com>
2023-08-28 09:38:47 +0800

    8.28(laptop)

--- src/main/java/com/dreamchaser/depository_manage/controller/ReportController.java
+++ src/main/java/com/dreamchaser/depository_manage/controller/ReportController.java
@@ -1,4 +1,5 @@
 package com.dreamchaser.depository_manage.controller;
+
 import com.dreamchaser.depository_manage.service.ReportService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.Map;
+import java.util.function.Function;
 
 @RestController
 @RequestMapping("/api")
@@ -22,38 +24,36 @@ public class ReportController {
             @RequestParam(name = "year", required = false, defaultValue = "-1") int year,
             @RequestParam(name = "month", required = false, defaultValue = "-1") int month,
             @RequestParam(name = "depositoryId") int depositoryId) {
-        if (year == -1) {
-            year = LocalDate.now().getYear();
-        }
-        if (month == -1) {
-            month = LocalDate.now().getMonthValue();
-        }
-        return reportService.fetchReportData( year,  month,  depositoryId);
+        return fetchData(year, month, date -> reportService.fetchReportData(date[0], date[1], depositoryId));
     }
+
     @GetMapping("/every")
     public List<Map<String, Object>> everyTypeData(
             @RequestParam(name = "year", required = false, defaultValue = "-1") int year,
             @RequestParam(name = "month", required = false, defaultValue = "-1") int month,
             @RequestParam(name = "depositoryId") int depositoryId) {
-        if (year == -1) {
-            year = LocalDate.now().getYear();
-        }
-        if (month == -1) {
-            month = LocalDate.now().getMonthValue();
-        }
-        return reportService.everyTypeData(year, month , depositoryId);
+        return fetchData(year, month, date -> reportService.everyTypeData(date[0], date[1], depositoryId));
     }
+
     @GetMapping("/transfer")
     public List<Map<String, Object>> transferData(
             @RequestParam(name = "year", required = false, defaultValue = "-1") int year,
-            @RequestParam(name = "month", required = false, defaultValue = "-1") int month){
+            @RequestParam(name = "month", required = false, defaultValue = "-1") int month) {
+        return fetchData(year, month, date -> reportService.transferData(date[0], date[1]));
+    }
+
+    private List<Map<String, Object>> fetchData(int year, int month, Function<int[], List<Map<String, Object>>> fetcher) {
+        int[] currentYearMonth = setCurrentYearAndMonth(year, month);
+        return fetcher.apply(currentYearMonth);
+    }
+
+    private int[] setCurrentYearAndMonth(int year, int month) {
         if (year == -1) {
             year = LocalDate.now().getYear();
         }
         if (month == -1) {
             month = LocalDate.now().getMonthValue();
         }
-        return reportService.transferData(year, month);
+        return new int[]{year, month};
     }
 }
-
