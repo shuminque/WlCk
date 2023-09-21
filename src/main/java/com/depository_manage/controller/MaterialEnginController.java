cc2b2de41abbbd539c7364f289a3c17e817f0497
Q <878576256@qq.com>
2023-09-21 10:06:27 +0800

    9.21(laptop)

--- src/main/java/com/depository_manage/controller/MaterialEnginController.java
+++ src/main/java/com/depository_manage/controller/MaterialEnginController.java
@@ -4,6 +4,8 @@ import com.depository_manage.pojo.RestResponse;
 import com.depository_manage.service.MaterialEnginService;
 import com.depository_manage.utils.CrudUtil;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
@@ -13,8 +15,13 @@ public class MaterialEnginController {
     @Autowired
     MaterialEnginService materialEnginService;
     @PostMapping("/materialEngin")
-    public RestResponse insertMaterialEngin(@RequestBody Map<String,Object> map){
-        return CrudUtil.postHandle(materialEnginService.insertMaterialEngin(map),1);
+    public ResponseEntity<RestResponse> insertMaterialEngin(@RequestBody Map<String,Object> map){
+        try {
+            RestResponse response = CrudUtil.postHandle(materialEnginService.insertMaterialEngin(map),1);
+            return ResponseEntity.status(HttpStatus.OK).body(response);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new RestResponse("添加错误，检查属性", 500, null));
+        }
     }
     @GetMapping("/materialEngin")
     public RestResponse findMaterialEngin(@RequestParam Map<String,Object> map){
--- src/main/java/com/depository_manage/controller/MaterialTypeController.java
+++ src/main/java/com/depository_manage/controller/MaterialTypeController.java
@@ -4,6 +4,8 @@ import com.depository_manage.pojo.RestResponse;
 import com.depository_manage.service.MaterialTypeService;
 import com.depository_manage.utils.CrudUtil;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
@@ -13,8 +15,13 @@ public class MaterialTypeController {
     @Autowired
     MaterialTypeService materialTypeService;
     @PostMapping("/materialType")
-    public RestResponse insertMaterialType(@RequestBody Map<String,Object> map){
-        return CrudUtil.postHandle(materialTypeService.insertMaterialType(map),1);
+    public ResponseEntity<RestResponse> insertMaterialType(@RequestBody Map<String,Object> map){
+        try {
+            RestResponse response = CrudUtil.postHandle(materialTypeService.insertMaterialType(map),1);
+            return ResponseEntity.status(HttpStatus.OK).body(response);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new RestResponse("添加错误，检查属性", 500, null));
+        }
     }
     @GetMapping("/materialType")
     public RestResponse findMaterialType(@RequestParam Map<String,Object> map){
