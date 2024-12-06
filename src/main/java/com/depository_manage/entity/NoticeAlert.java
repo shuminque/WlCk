9aed5b84e3cce66a918ae99b5e255dfbb3a5f5af
Q <878576256@qq.com>
2024-12-06 15:21:24 +0800

    12.06(laptop)

--- src/main/java/com/depository_manage/entity/NoticeAlert.java
+++ src/main/java/com/depository_manage/entity/NoticeAlert.java
@@ -7,4 +7,7 @@ public class NoticeAlert {
     private Integer id;            // 自增ID
     private Integer atId;          // 物品ID
     private Integer alertQuantity; // 预警数量
+    private String mname;
+    private String model;
+
 }
